3d3abec5cf7bc31faaa9bf137698a4e457516298
Chris McGehee <chris.mcgehee@gmail.com>
2022-02-03 13:48:40 -0800

    Added FTS5 support to package

--- Package.swift
+++ Package.swift
@@ -23,7 +23,10 @@ let package = Package(
         .target(
             name: "GRDB",
             dependencies: ["CSQLite"],
-            path: "GRDB"),
+            path: "GRDB",
+            swiftSettings: [
+                .define("SQLITE_ENABLE_FTS5")
+            ]),
         .testTarget(
             name: "GRDBTests",
             dependencies: ["GRDB"],
